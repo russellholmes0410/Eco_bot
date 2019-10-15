12718fc15da08b9e98728757b13b629c3ffecae4
russellholmes0410 <56272617+russellholmes0410@users.noreply.github.com>
2019-10-15 19:33:26 +0200

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=patientjourney-8b7a-Web-Deploy.pubxml -p:Password=1sJ2fZcCW2bqEpRcZ8YPwRX1tblkR2x5ZbQPNbWQclZPWYxoSrKdQ2nkwbeS
+
